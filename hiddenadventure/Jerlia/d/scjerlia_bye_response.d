62f7f22b5b558fbc2977dcc2d243ecf0d7c3a3e1
Ychap <asdfsa85@web.de>
2024-11-23 17:16:55 +0100

    Do not go from state 37 to state 2 in SCJERLIA.DLG
    State 2 brings us to an unwanted action: SetNumTimesTalkedTo(0)

--- hiddenadventure/Jerlia/d/scjerlia_bye_response.d
+++ hiddenadventure/Jerlia/d/scjerlia_bye_response.d
@@ -3,5 +3,12 @@
 // Should only be added once
 ADD_TRANS_TRIGGER ~scjerlia~ 37 ~False()~ DO 0 UNLESS ~"False()"~
 EXTEND_TOP ~scjerlia~ 37 #1
-  ++ #28743 /* Nothing, sorry I bothered you. */ + 2
+  ++ #28743 /* Nothing, sorry I bothered you. */ + SCJERLIA01
+END
+
+APPEND SCJERLIA
+	IF ~~ THEN BEGIN SCJERLIA01
+		SAY #28746 /* S'all right friend. Come back when you need. */
+		IF ~~ THEN EXIT
+	END
 END
